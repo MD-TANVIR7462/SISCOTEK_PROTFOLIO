6b8d31644dd60da70aa39df1dda1f8e35af5688a
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2024-11-01 12:10:04 +0600

    working on theme  section

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,51 +1,25 @@
 "use client";
 
-import type { NewsletterEmail } from "@/types";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableColumn,
-  TableHeader,
-  TableRow,
-} from "@nextui-org/table";
-import { format } from "date-fns";
-import { ConfirmationModal } from "./confirmation-modal";
+import { Switch } from "@nextui-org/switch";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
-type NewsletterTableProps = {
-  emails: NewsletterEmail[];
-};
+export const ThemeSwitcher = () => {
+  const { theme, setTheme } = useTheme();
 
-export function NewsletterTable({ emails }: Readonly<NewsletterTableProps>) {
   return (
-    <Table
-      classNames={{
-        wrapper:
-          "bg-background border border-foreground/10 dark:bg-background/60 dark:data-[hover=true]:bg-background/80 backdrop-blur-md backdrop-saturate-150",
-        th: "text-base",
-        td: "text-base",
-      }}
-    >
-      <TableHeader>
-        <TableColumn>Email</TableColumn>
-        <TableColumn>Submitted On</TableColumn>
-        <TableColumn>Actions</TableColumn>
-      </TableHeader>
-      <TableBody>
-        {emails.map(({ id, email, date }) => (
-          <TableRow key={id}>
-            <TableCell>
-              <h4 className="font-medium">{email}</h4>
-            </TableCell>
-            <TableCell>
-              <h4 className="font-medium">{format(date, "PP")}</h4>
-            </TableCell>
-            <TableCell>
-              <ConfirmationModal onConfirm={() => console.log(id)} />
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <Switch
+      isSelected={theme === "dark"} // Set selected based on the current theme
+      size="md"
+      color="primary" 
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      thumbIcon={({ isSelected, className }) =>
+        isSelected ? (
+          <Moon className={className} size={14} />
+        ) : (
+          <Sun className={className} size={14} />
+        )
+      }
+    />
   );
-}
+};
